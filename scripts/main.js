321d0acb076cc9570b5b9850ed9974550933003f
Luke Higgott <higgottluke@gmail.com>
2017-03-19 20:18:12 -0700

    finished chapter 11

--- scripts/main.js
+++ scripts/main.js
@@ -2,14 +2,21 @@
 (function (window) {
   'use strict';
   var FORM_SELECTOR = '[data-coffee-order="form"]';
+  var CHECKLIST_SELECTOR = '[data-coffee-order="checklist"]';
   var App = window.App;
   var Truck = App.Truck;
   var DataStore = App.DataStore;
   var FormHandler = App.FormHandler;
+  var CheckList = App.CheckList;
   var myTruck = new Truck('ncc-1701', new DataStore());
   window.myTruck = myTruck;
+  var checkList = new CheckList(CHECKLIST_SELECTOR);
+  checkList.addClickHandler(myTruck.deliverOrder.bind(myTruck));
   var formHandler = new FormHandler(FORM_SELECTOR);
-  formHandler.addSubmitHandler(myTruck.createOrder.bind(myTruck));
+  formHandler.addSubmitHandler(function (data) {
+    myTruck.createOrder.call(myTruck, data);
+    checkList.addRow.call(checkList, data);
+  });
   console.log(formHandler);
 
 })(window);
@@ -32,7 +39,7 @@ if ($slider) {
     }
   });
 }
-
+/*
 // Gold Challenge
 $('[data-coffee-order]="form"').on('change', function () {
   if (form[0][1].value && form[0][2].value == 'trenta' && form[0][3].value && form[0][4]
@@ -42,3 +49,4 @@ modal.modal();
 if (data['emailAddress']) {
   $('[data-achiev-form]').toggle();
 }
+*/
